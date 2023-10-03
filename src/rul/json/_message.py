76430902dddb0ba7bef13cc036ac9b20bb875ad7
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2023-10-03 15:30:12 +0200

    added methode JsonMessage.getMessage() tested by test_getMessage()

--- src/rul/json/_message.py
+++ src/rul/json/_message.py
@@ -20,7 +20,32 @@ class JsonMessage(AbstractMessage):
             pass
 
     def getMessages(self) -> List[object]:
-        pass
+        """Retrieve all Message
+
+        Returns:
+            List[object]: list of message representative objects in form:
+                          { "id": id, "header": header, "body": body }
+        """
+        with open(self.json_file, "r") as openfile:
+            messages = json.load(openfile)
+
+        return messages
 
     def postMessage(self, header: str, body: str) -> None:
-        pass
+        """Post new message
+
+        Args:
+            header (str): message header text
+            body (str): message body text
+        """
+
+        new_msg = {
+            "id": 0,
+            "header": header,
+            "body": body
+        }
+
+        json_dump = json.dumps(new_msg)
+
+        with open(self.json_file, "w") as writefile:
+            writefile.write(json_dump)
--- tests/json/test_JsonMessage.py
+++ tests/json/test_JsonMessage.py
@@ -13,3 +13,23 @@ def test_initialize():
     jsonMsg.initialize()
     assert os.path.exists("test.json")
     os.remove("test.json")
+
+
+def test_getMessage():
+    jsonMsg = JsonMessage("test.json")
+    jsonMsg.initialize()
+    with open("test.json", "w") as writefile:
+        writefile.write("[{\"id\": 0, \"header\": \"Head\", \"body\": \"Body\"}]")
+    msg = jsonMsg.getMessages()
+    assert msg == [{"id": 0, "header": "Head", "body": "Body"}]
+    os.remove("test.json")
+
+
+def test_postMessage():
+    jsonMsg = JsonMessage("test.json")
+    jsonMsg.initialize()
+    jsonMsg.postMessage("Head", "Body")
+    with open("test.json", "r") as outfile:
+        content = outfile.read()
+    assert content == "{\"id\": 0, \"header\": \"Head\", \"body\": \"Body\"}"
+    os.remove("test.json")
